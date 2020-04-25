1584c351de047bde5743afde098b8aa70b5e2ef9
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-04-25 23:52:59 +0300

    Short Declration Opreator

---
+++ B_Variables_Values_Types/ShortDeclrationOperator/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 50
+	fmt.Println(x)
+	y := 50 + x
+	fmt.Println(y)
+	z := "Is a string"
+	fmt.Println((z))
+}
+
+/**
+1) short declration variable :=
+2) you must use a declared variable.
+3) assign a variable has also type safe.
+*/
